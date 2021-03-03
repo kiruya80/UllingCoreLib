143ddce72895586d0ab44094306506fda51622e2
kiruya80@gmail.com <kilho3050>
2021-03-03 16:30:42 +0900

    버튼 클릭 중복 방지 수정

--- Sample/src/main/java/com/ulling/sample/App.java
+++ Sample/src/main/java/com/ulling/sample/App.java
@@ -4,4 +4,11 @@ import com.ulling.lib.core.base.QcBaseApplication;
 
 public class App extends QcBaseApplication {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CLICK_INTERVAL = 2000l;
+        IS_CLICK_ALL = true;
+    }
 }
